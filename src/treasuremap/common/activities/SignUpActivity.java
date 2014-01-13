2f0d5f1ecffcc21b7ab0d9531e48c9eb24ce1d02
memeoo <memehsforever@gmail.com>
2014-01-13 14:32:54 +0900

    commit for SignUpActivity

---
+++ src/treasuremap/common/activities/SignUpActivity.java
@@ -0,0 +1,14 @@
+package treasuremap.common.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+}
