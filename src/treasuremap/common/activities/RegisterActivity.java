27428c7889d42fa5f4cb0d05fe60d93dbdfce026
memeoo <memehsforever@gmail.com>
2014-01-15 11:59:45 +0900

    Commit

--- src/treasuremap/common/activities/RegisterActivity.java
+++ src/treasuremap/common/activities/RegisterActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class RegisterActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ src/treasuremap/common/general/Tag.java
@@ -0,0 +1,5 @@
+package treasuremap.common.general;
+
+public class Tag {
+
+}
